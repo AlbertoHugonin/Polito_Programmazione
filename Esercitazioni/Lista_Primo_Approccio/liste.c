d5070333a396a3fafae8bec82824a92c6b63bb00
Alberto <alberto.hugonin@icloud.com>
2020-11-29 15:05:31 +0100

    versione definitiva lista ordinata

--- Esercitazioni/Lista_Primo_Approccio/liste.c
+++ Esercitazioni/Lista_Primo_Approccio/liste.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//massima lunghezza stringa
 #define MAX_N 30
 
 
@@ -35,14 +36,13 @@ database_stringhe *carica_lista() {
     FILE *ptr = fopen("lista_stringhe", "r");
 
     char *elemento = malloc(MAX_N * sizeof(char));
-    database_stringhe *cache = malloc(sizeof(database_stringhe));
+    database_stringhe *cache = (database_stringhe *)malloc(sizeof(database_stringhe));
     cache->testa = NULL;
     cache->num=0;
     //inserimento primo elemento
-    fscanf(ptr,"%s", elemento);
-    inserisci_in_lista(cache, elemento);
-    fscanf(ptr,"%s", elemento);
-    inserisci_in_lista(cache, elemento);
+    while (fscanf(ptr,"%s", elemento) == 1) {
+        inserisci_in_lista(cache, elemento);
+    }
     return cache;
 }
 
@@ -53,11 +53,7 @@ int inserisci_in_lista (database_stringhe *cache, char *elemento) {
 
     //prepara il nuovo nodo con la sua stringa
     struct riga *nodo = (struct riga *) malloc (sizeof(struct riga));
-    nodo->stringa = malloc(sizeof(elemento) * sizeof(char));
-
-    for (int a=0; a<sizeof(elemento); a++) {
-        nodo->stringa[a] = elemento[a];
-    }
+    nodo->stringa = strdup(elemento);
 
     struct riga *corrente = cache->testa;
     struct riga *precedente = NULL;
@@ -68,15 +64,21 @@ int inserisci_in_lista (database_stringhe *cache, char *elemento) {
         cache->testa = nodo;
     }
     else {
-        //inserimenti successivi
-        while (corrente != NULL && strcmp(elemento,corrente->stringa) < 0)  {
-            //siamo ad elemento e prima di esso ci va questo
-            
-            nodo->next=rif;
-
-            return 1;
+        //continuiamo a scorrere la lista finche non troviamo l'elemento giusto
+        while (corrente != NULL && strcmp(elemento,corrente->stringa) > 0)  {
+            precedente = corrente;
+            corrente = corrente->next;
         }
-        
+        //trovato il posto giusto facciamo distinzione se siamo al primo elemento dopo la testa o no
+        if (precedente==NULL) {
+            cache->testa = nodo;
+            nodo->next=corrente;
+        }
+        else {
+            //caso generale centrale
+            precedente->next=nodo;
+            nodo->next=corrente;
+        }
     }
     cache->num++;
     return 1;
