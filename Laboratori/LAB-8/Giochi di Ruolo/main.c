bfd209b956c422c5ab6e3c1b229a5f7fe787fab4
Alberto <alberto.hugonin@icloud.com>
2020-12-11 19:00:24 +0100

    prima versione completa giochi di ruolo

--- Laboratori/LAB-8/Giochi di Ruolo/main.c
+++ Laboratori/LAB-8/Giochi di Ruolo/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_INV 50
 //in una lista l'elenco dei personaggi
 //vettore di strutture l'elenco di oggetti
 
@@ -24,7 +25,7 @@ typedef struct elem_inv_ {
     char *tipo;
     stat statistiche;
 
-} elem_inv;
+} elem_inv, vett_equi;
 
 typedef struct tab_inv_ {
 
@@ -33,6 +34,35 @@ typedef struct tab_inv_ {
 
 } tab_inv;
 
+//////////////////////////////////
+
+typedef struct tab_equi_ {
+
+    int inUso;
+    vett_equi *vett_equipaggiamento;
+
+} tab_equi;
+
+typedef struct pers_ {
+
+    char *codice;
+    char *nome;
+    char *classe;
+    stat statistiche;
+    tab_equi tab_equipaggiamento;
+
+    struct pers_ *next;
+
+} pers;
+
+//wrapper della lista personaggi
+typedef struct tab_pers_ {
+
+    pers *head;
+    pers *tail;
+    int n_personaggi;
+
+} tab_pers;
 
 //////////////////////////////////////
 
@@ -41,6 +71,12 @@ tab_inv *crea_inventario();
 void aggiungi_elemento_inventario(FILE *ptr, tab_inv *inventario, int a);
 int lunghezza_stringa(char *stringa, int max);
 
+///////////////////////////////////////
+tab_pers *crea_tabella_personaggi();
+void carica_personaggi_da_file( tab_pers *tabella_personaggi, FILE *file);
+pers crea_pers_personalizzato();
+pers *aggiungi_personaggio(pers personaggio, tab_pers *tab_personaggi, pers *aggiunto);
+void formatta_stringhe_pers(pers *personaggio, char *codice, char *nome, char *classe);
 //////////////////////////////////////
 
 
@@ -53,7 +89,129 @@ int main() {
 
     tabella_inventario = carica_inventario(file_inventario,tabella_inventario);
 
+    //era anche possibile nascondere il puntatore alla funzione main (vedere parte modularità)
+    tab_pers *tabella_personaggi = crea_tabella_personaggi();
+
+    FILE *file_personaggi = fopen("pg.txt", "r");
+
+    carica_personaggi_da_file(tabella_personaggi,file_personaggi);
+
+    //crea personaggio (puo' esere usata anche dalla carica personaggi)
+    pers personaggio = crea_pers_personalizzato();
+
+    //aggiungi personaggio
+    aggiungi_personaggio(personaggio,tabella_personaggi, NULL);
+
     return 0;
+}
+
+pers crea_pers_personalizzato() {
+
+    pers *personaggio = malloc(sizeof(pers));
+
+    int n;
+
+    char codice[MAX_INV];
+    char nome[MAX_INV];
+    char classe[MAX_INV];
+
+    puts("Inserisci le seguenti informazioni:");
+    puts("< Codice > < Nome > < Classe > < HP > < MP > < ATK > < DEF > < MAG > < SPR >");
+    fflush(stdin);
+    fscanf(stdin, "%s %s %s %d %d %d %d %d %d", codice, nome, classe, &personaggio->statistiche.hp, &personaggio->statistiche.mp, &personaggio->statistiche.atk, &personaggio->statistiche.def, &personaggio->statistiche.mag, &personaggio->statistiche.spr);
+    formatta_stringhe_pers(personaggio,codice,nome,classe);
+    return *personaggio;
+}
+
+//si occupa di prendere i dati da file, creare con essi un personaggio e farlo aggiungere alla funziona aggiungi personaggio
+void carica_personaggi_da_file(tab_pers *tabella_personaggi, FILE *file) {
+
+    //creiamo il primo personaggio
+    pers *personaggio = malloc(sizeof(pers));
+    pers *aggiunto = NULL;
+
+    int n;
+
+    char codice[MAX_INV];
+    char nome[MAX_INV];
+    char classe[MAX_INV];
+
+    while (fscanf(file, "%s %s %s %d %d %d %d %d %d", codice, nome, classe, &personaggio->statistiche.hp, &personaggio->statistiche.mp, &personaggio->statistiche.atk, &personaggio->statistiche.def, &personaggio->statistiche.mag, &personaggio->statistiche.spr) != EOF)
+    {
+        formatta_stringhe_pers(personaggio,codice,nome,classe);
+        aggiunto = aggiungi_personaggio(*personaggio, tabella_personaggi, aggiunto);
+        free(personaggio);
+        personaggio = malloc(sizeof(pers));
+    }
+
+    return;
+}
+
+void formatta_stringhe_pers(pers *personaggio, char *codice, char *nome, char *classe)
+{
+    int n;
+    n = lunghezza_stringa(codice, MAX_INV);
+    personaggio->codice = malloc(n * sizeof(char));
+    memcpy(personaggio->codice, codice, n * sizeof(char));
+    ///////
+    n = lunghezza_stringa(nome, MAX_INV);
+    personaggio->nome = malloc(n * sizeof(char));
+    memcpy(personaggio->nome, nome, n * sizeof(char));
+    ///////
+    n = lunghezza_stringa(classe, MAX_INV);
+    personaggio->classe = malloc(n * sizeof(char));
+    memcpy(personaggio->classe, classe, n * sizeof(char));
+    personaggio->next = NULL;
+}
+
+pers *aggiungi_personaggio(pers personaggio, tab_pers *tab_personaggi, pers *aggiunto) {
+
+    //questa malloc risolve tutto, senza memcpy è come se copiasse il puntatore e non il contenuto (non è la strdup che f anche la malloc)
+    pers *nodo = malloc(sizeof(pers));
+    memcpy(nodo,&personaggio,sizeof(pers));
+    nodo->next=NULL;
+
+    if (tab_personaggi->head == NULL) {
+        tab_personaggi->head=nodo;
+        tab_personaggi->tail=nodo;
+        tab_personaggi->n_personaggi++;
+        return nodo;
+    }
+    else {
+        //versione solo inserimento in coda
+        if (aggiunto != NULL) {
+            //usiamo il riferimento
+            aggiunto->next=nodo;
+            tab_personaggi->tail=nodo;
+            tab_personaggi->n_personaggi++;
+            return nodo;
+        } else {
+            //allora dobbiamo scorrere la lista
+            pers *corrente = tab_personaggi->head;
+            while (corrente->next != NULL) {
+                corrente = corrente->next;
+            }
+            corrente->next=nodo;
+            tab_personaggi->tail=nodo;
+            tab_personaggi->n_personaggi++;
+            return nodo;
+        }
+
+    }
+
+}
+
+
+
+tab_pers *crea_tabella_personaggi() {
+    
+    tab_pers *tab_personaggi = malloc(sizeof(tab_pers));
+    
+    tab_personaggi->n_personaggi = 0;
+    tab_personaggi->head = NULL;
+    tab_personaggi->tail = NULL;
+
+    return tab_personaggi;
 }
 
 
@@ -88,15 +246,15 @@ void aggiungi_elemento_inventario(FILE *ptr, tab_inv *inventario, int a) {
     elem_inv *corrente = inventario->vett_inventario+a;
 
     int n;
-    char cache[50];
+    char cache[MAX_INV];
 
-    fscanf(ptr,"%s", &cache);
-    n = lunghezza_stringa(cache,50);
+    fscanf(ptr,"%s", cache);
+    n = lunghezza_stringa(cache,MAX_INV);
     corrente->nome = malloc(n*sizeof(char));
     memcpy(corrente->nome,cache,n*sizeof(char));
 
-    fscanf(ptr,"%s", &cache);
-    n = lunghezza_stringa(cache,50);
+    fscanf(ptr,"%s", cache);
+    n = lunghezza_stringa(cache,MAX_INV);
     corrente->tipo = malloc(n*sizeof(char));
     memcpy(corrente->tipo,cache,n*sizeof(char));
 
