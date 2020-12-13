6ac5ee24db26924569c5735f3f54289e73685658
Alberto <alberto.hugonin@icloud.com>
2020-12-13 13:21:05 +0100

    versione finale giochi di ruolo non modulare

--- Laboratori/LAB-8/Giochi di Ruolo/main.c
+++ Laboratori/LAB-8/Giochi di Ruolo/main.c
@@ -77,6 +77,17 @@ void carica_personaggi_da_file( tab_pers *tabella_personaggi, FILE *file);
 pers crea_pers_personalizzato();
 pers *aggiungi_personaggio(pers personaggio, tab_pers *tab_personaggi, pers *aggiunto);
 void formatta_stringhe_pers(pers *personaggio, char *codice, char *nome, char *classe);
+
+void aggiungi_equipaggiamento(tab_pers *tab_personaggi, tab_inv *tab_inventario, char *codice_pers, char *nome_oggetto);
+
+pers elimina_personaggio(tab_pers *tab_personaggi, char *codice_pers);
+
+void calcola_statistiche(tab_pers *tab_personaggi, char *codice_pers);
+
+pers *trova_personaggio(tab_pers *tab_personaggi, char *codice_pers);
+
+int trova_oggetto_equipaggiamento(pers *personaggio, char *nome_oggetto);
+int trova_oggetto_inventario(tab_inv *tab_inventario, char *nome_oggetto);
 //////////////////////////////////////
 
 
@@ -97,12 +108,151 @@ int main() {
     carica_personaggi_da_file(tabella_personaggi,file_personaggi);
 
     //crea personaggio (puo' esere usata anche dalla carica personaggi)
-    pers personaggio = crea_pers_personalizzato();
+    //pers personaggio = crea_pers_personalizzato();
 
     //aggiungi personaggio
-    aggiungi_personaggio(personaggio,tabella_personaggi, NULL);
+    //aggiungi_personaggio(personaggio,tabella_personaggi, NULL);
 
+    elimina_personaggio(tabella_personaggi, "PG0001");
+
+    //elimina personaggio, se glielo passiamo elimina quello se mettiamo una stringa cerchiamo il suo id
+    //elimina_personaggio(tabella_personaggi, personaggio.codice);
+
+    aggiungi_equipaggiamento(tabella_personaggi, tabella_inventario, "PG0002", "Escutcheon");
+    aggiungi_equipaggiamento(tabella_personaggi, tabella_inventario, "PG0002", "Escutcheon");
+    aggiungi_equipaggiamento(tabella_personaggi, tabella_inventario, "PG0002", "TalismanoNero");
+    calcola_statistiche(tabella_personaggi,"PG0002");
     return 0;
+}
+
+void calcola_statistiche(tab_pers *tab_personaggi, char *codice_pers) {
+
+    pers *personaggio = trova_personaggio(tab_personaggi,codice_pers);
+    stat statistiche_base;
+
+    memcpy(&statistiche_base,&personaggio->statistiche,sizeof(stat));
+
+    stat *equi_corrente;
+
+
+    for (int a=0; a<personaggio->tab_equipaggiamento.inUso; a++) {
+
+        equi_corrente = &personaggio->tab_equipaggiamento.vett_equipaggiamento[a].statistiche;
+
+        statistiche_base.hp += equi_corrente->hp;
+        statistiche_base.atk += equi_corrente->atk;
+        statistiche_base.def += equi_corrente->def;
+        statistiche_base.mag += equi_corrente->mag;
+        statistiche_base.mp += equi_corrente->mp;
+        statistiche_base.spr += equi_corrente->spr;
+    }
+
+    printf("Statistiche di %s sono: HP < %d > MP < %d > ATK < %d > DEF < %d > MAG < %d > SPR < %d >", codice_pers, statistiche_base.hp, statistiche_base.mp, statistiche_base.atk, statistiche_base.def, statistiche_base.mag, statistiche_base.spr);
+    fflush(stdin);
+    return;
+}
+
+
+pers *trova_personaggio(tab_pers *tab_personaggi, char *codice_pers) {
+
+    pers *corrente = tab_personaggi->head;
+
+    while (corrente->next != NULL && strcmp(corrente->codice,codice_pers) != 0) {
+        corrente=corrente->next;
+    }
+    if (strcmp(corrente->codice,codice_pers) != 0) {
+        puts("Personaggio non trovato");
+        return NULL;
+    }
+
+    return corrente;
+}
+
+int trova_oggetto_inventario(tab_inv *tab_inventario, char *nome_oggetto) {
+
+    int pos;
+    for (pos = 0; pos<tab_inventario->n; pos++) {
+        if (strcmp(tab_inventario->vett_inventario[pos].nome,nome_oggetto) == 0) {
+            return pos;
+        }
+    }
+    return -1;
+
+}
+
+int trova_oggetto_equipaggiamento(pers *personaggio, char *nome_oggetto) {
+
+    int pos;
+    for (pos = 0; pos<personaggio->tab_equipaggiamento.inUso; pos++) {
+        if (strcmp(personaggio->tab_equipaggiamento.vett_equipaggiamento[pos].nome,nome_oggetto) == 0) {
+            return pos;
+        }
+    }
+    return -1;
+
+}
+
+void aggiungi_equipaggiamento(tab_pers *tab_personaggi, tab_inv *tab_inventario, char *codice_pers, char *nome_oggetto) {
+
+    pers *corrente = trova_personaggio(tab_personaggi,codice_pers);
+
+    if (corrente == NULL) {
+        puts("Personaggio  non trovato, equipaggiamento non aggiornato");
+        return;
+    }
+
+    int pos = trova_oggetto_inventario(tab_inventario, nome_oggetto);
+
+    if (pos == -1) {
+        puts("Oggetto non trovato, equipaggiamento non aggiornato");
+        return;
+    }
+
+    tab_equi *tab_equipaggiamento = &corrente->tab_equipaggiamento;
+
+    if (trova_oggetto_equipaggiamento(corrente, nome_oggetto) != -1) {
+        //oggetto già presente dentro equipaggiamento
+        puts("Oggetto già presente dentro equipaggiamento, non verrà aggiunto");
+        return;
+    }
+
+    vett_equi *nuovo_equip = malloc((tab_equipaggiamento->inUso+1)*sizeof(vett_equi));
+    memcpy(nuovo_equip,tab_equipaggiamento->vett_equipaggiamento,tab_equipaggiamento->inUso*sizeof(vett_equi));
+    nuovo_equip[tab_equipaggiamento->inUso] = tab_inventario->vett_inventario[pos];
+
+    tab_equipaggiamento->vett_equipaggiamento=nuovo_equip;
+    tab_equipaggiamento->inUso++;
+
+    return;
+
+}
+
+pers elimina_personaggio(tab_pers *tab_personaggi, char *codice_pers) {
+
+    pers *corrente = tab_personaggi->head;
+    pers *ritorno;
+
+    if (strcmp(corrente->codice,codice_pers) == 0) {
+        tab_personaggi->head=corrente->next;
+        corrente->next=NULL;
+        tab_personaggi->n_personaggi--;
+        return *corrente;
+    }
+
+    while (corrente->next != NULL && strcmp(corrente->next->codice,codice_pers) != 0) {
+        corrente=corrente->next;
+    }
+    //backup del personaggio
+    ritorno=corrente->next;
+    //bypassiamo il nodo
+    corrente->next=ritorno->next;
+    //aggiornamento coda
+    if (tab_personaggi->tail == ritorno) {
+        tab_personaggi->tail=corrente;
+    }
+    ritorno->next=NULL;
+
+    return *ritorno;
 }
 
 pers crea_pers_personalizzato() {
