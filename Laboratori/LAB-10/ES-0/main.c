0fd840e8f32259f9b73831f1a0b41953a877283d
AlbertoHugonin <alberto.hugonin@icloud.com>
2021-02-20 19:49:23 +0100

    inizio antenne

---
+++ Laboratori/LAB-10/ES-0/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    int n = 7;
+    int antenne = { 0, 14, 22 , 13 , 25 , 30 , 11 , 90 };
+    //vett soluzione ottima scandito da indice k
+    int opt[n+1];
+    int k;
+
+}
